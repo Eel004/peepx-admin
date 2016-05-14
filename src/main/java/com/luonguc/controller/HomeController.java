dce420616b8240b81675f03213c67589d465e93a
LuongUC <luonguc93@gmail.com>
2016-05-15 00:41:47 +0700

    Revert "try changed"
    This reverts commit 2edcc4f4411a4090df2ef80f378a1d921d2210db.

--- src/main/java/com/luonguc/controller/HomeController.java
+++ src/main/java/com/luonguc/controller/HomeController.java
@@ -39,7 +39,7 @@ public class HomeController {
 	@Autowired private TeamService teamService;
 	@Autowired private PositionService positionService;
 	
-	// Home - index
+	// home
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json",
 			headers = "Accept=application/json")
 	public String home(Model model) {
