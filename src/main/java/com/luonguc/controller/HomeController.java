e117f9718a66ae7959e5eb978bed21e90c163bf0
LuongUC <luonguc93@gmail.com>
2016-05-15 01:00:14 +0700

    try

--- src/main/java/com/luonguc/controller/HomeController.java
+++ src/main/java/com/luonguc/controller/HomeController.java
@@ -39,7 +39,7 @@ public class HomeController {
 	@Autowired private TeamService teamService;
 	@Autowired private PositionService positionService;
 	
-	// home
+	// home - index 1
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json",
 			headers = "Accept=application/json")
 	public String home(Model model) {
